83452f2079f888b84c99081d199dc57b62f3f46f
enkhbold470 <enkhbold470@gmail.com>
2024-10-05 23:54:52 -0400

    feat: Update favicon and manifest for GreenMate website

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -16,7 +16,6 @@ export const metadata: Metadata = {
   title: "GreenMate",
   description:
     "GreenMate is a platform for managing greenhouses and plants. We are leveraging IoT Sensors with ESP32 and AI to help you grow your plants. Whether you're new to planting or an experienced grower, we've got you covered.",
-  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -26,6 +25,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-48x48.png"
+          sizes="48x48"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="GreenMate" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${roboto.className} antialiased`}>
         {children}
         <BottomNavbar />
